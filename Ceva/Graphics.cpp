6389d885d378cc51b7488b041ab4db4f9b7e16cc
Stefan <sdobrica188@gmail.com>
2017-02-11 10:58:15 +0200

    Added a forest using billboarding

--- Ceva/Graphics.cpp
+++ Ceva/Graphics.cpp
@@ -17,21 +17,6 @@ bool CGraphics::Initialize( HINSTANCE hInstance, HWND hWnd, UINT WindowWidth, UI
 	m_Cursor = new BitmapClass();
 	if (!m_Cursor->Initialize( m_d3d->GetDevice(), L"data\\Cursor.dds", WindowWidth, WindowHeight, 32, 32 ))
 		return false;
-	m_Wall = new CModel( );
-	if ( !m_Wall->Initialize( m_d3d->GetDevice( ), L"data\\wall.txt", L"data\\wall01.dds" ) )
-		return false;
-	m_Ground = new CModel( );
-	if ( !m_Ground->Initialize( m_d3d->GetDevice( ), L"data\\ground.txt", L"data\\ground01.dds" ) )
-		return false;
-	m_Bath = new CModel( );
-	if ( !m_Bath->Initialize( m_d3d->GetDevice( ), L"data\\bath.txt", L"data\\marble01.dds" ) )
-		return false;
-	m_Glass = new CModel( );
-	if ( !m_Glass->Initialize( m_d3d->GetDevice( ), L"data\\square.txt", L"data\\ice01.dds", L"data\\icebump01.dds" ) )
-		return false;
-	m_Water = new CModel( );
-	if ( !m_Water->Initialize( m_d3d->GetDevice( ), L"data\\water.txt", L"data\\water01.dds" ) )
-		return false;
 	m_NoPlaneClippingShader = new CSimpleShader();
 	if (!m_NoPlaneClippingShader->Initialize( m_d3d->GetDevice() ))
 		return false;
@@ -67,7 +52,7 @@ bool CGraphics::Initialize( HINSTANCE hInstance, HWND hWnd, UINT WindowWidth, UI
 		return false;
 	m_Camera = new CCamera();
 	if (!m_Camera->Initialize( DirectX::XMVectorSet( 0.0f, 0.0f, -10.0f, 1.0f ),
-		0.2f * FLOAT_PI, (FLOAT)WindowWidth / WindowHeight, 0.1f, 100.0f, 15.0f ))
+		0.2f * FLOAT_PI, (FLOAT)WindowWidth / WindowHeight, 0.1f, 10000.0f, 15.0f ))
 		return false;
 	if (!FontClass::Initialize( m_d3d->GetDevice(), L"data\\font.dds", L"data\\fontdata.txt" ))
 		return false;
@@ -81,19 +66,17 @@ bool CGraphics::Initialize( HINSTANCE hInstance, HWND hWnd, UINT WindowWidth, UI
 	m_FrameTimeMessage = new CSentence();
 	if ( !m_FrameTimeMessage->Initialize( m_d3d->GetDevice( ), "Frame time: 0.00", WindowWidth, WindowHeight, 1.0f, 20.0f ) )
 		return false;
-	m_RefractionTexture = new CRenderTexture();
-	if (!m_RefractionTexture->Initialize( m_d3d->GetDevice(), WindowWidth, WindowHeight ))
+	m_Square = new CModel( );
+	if ( !m_Square->Initialize( m_d3d->GetDevice( ), L"data\\square.txt", L"data\\Copac.png" ) )
 		return false;
-	m_ReflectionTexture = new CRenderTexture( );
-	if ( !m_ReflectionTexture->Initialize( m_d3d->GetDevice( ), WindowWidth, WindowHeight ) )
+	m_Ground = new CModel( );
+	if ( !m_Ground->Initialize( m_d3d->GetDevice( ), L"data\\ground.txt", L"data\\ground01.dds" ) )
 		return false;
-	m_GlassRefraction = new CRenderTexture( );
-	if ( !m_GlassRefraction->Initialize( m_d3d->GetDevice( ), WindowWidth, WindowHeight ) )
-		return false;
+	trees.resize( 600 );
 	Light = new CLight();
 	Light->SetSpecularColor( common::HexToRGB( 0xFFFFFF ) );
-	Light->SetAmbientColor( common::Color( 0.0f, 0.0f, 0.0f, 1.0f ) );
-	Light->SetDiffuseColor( common::Color( 0.3f, 0.3f, 0.3f, 1.0f ) );
+	Light->SetAmbientColor( common::Color( 1.0f, 1.0f, 1.0f, 1.0f ) );
+	Light->SetDiffuseColor( common::Color( 1.0f, 1.0f, 1.0f, 1.0f ) );
 	Light->SetDirection( DirectX::XMFLOAT3( 0.0f, -1.0f, 0.5f ) );
 	Light->SetSpecularPower( 32.0f );
 
@@ -103,47 +86,21 @@ bool CGraphics::Initialize( HINSTANCE hInstance, HWND hWnd, UINT WindowWidth, UI
 	PointLight->SetAttenuation( 0.0f, 0.2f, 0.0f );
 	PointLight->SetRange( 0.0f ); // disable point light
 
-	m_fWaterHeight = 0.75f;
-	m_fWaterTranslation = 0.0f;
-
-
-	m_fStartLoadingTime = 5;
-	m_fLoadingTime = 0;
-	m_bIncrease = true;
 	return true;
 }
 
 void CGraphics::Update( bool RenderMenu, DWORD dwFramesPerSecond, float fFrameTime, UINT MouseX, UINT MouseY, char * cheat )
 {
-	m_fWaterTranslation += 0.125f * fFrameTime;
-	if ( m_fWaterTranslation > 1.0f )
-		m_fWaterTranslation = 0.0f;
-	//PointLight->SetPosition( m_Camera->GetCameraPosition( ) );
-	m_Glass->Identity( );
-	m_Glass->Translate( 0.0f, 2.0f, -9.0f );
-	m_Bath->Identity( );
-	//m_Bath->Translate( 0.0f, 0.0f, 0.0f );
-	m_Wall->Identity( );
-	m_Wall->Translate( 0.0f, 4.0f, 8.0f );
+	PointLight->SetPosition( m_Camera->GetCameraPosition( ) );
 	m_Ground->Identity( );
-	m_Ground->Translate( 0.0f, -1.0f, 0.0f );
-	m_Water->Identity( );
-	m_Water->Translate( 0.0f, m_fWaterHeight, 0.0f );
+	m_Ground->Scale( 400.0f, 1.0f, 400.0f );
 	if (RenderMenu)
 	{
 		char buffer[500] = { 0 };
 		sprintf_s( buffer, "Frames per second: %d", dwFramesPerSecond );
 		m_FPSMessage->Update( m_d3d->GetImmediateContext( ), buffer, strlen( buffer ), 1.0f, 1.0f );
-		if (strcmp( cheat, "GetRenderCount" ) == 0)
-		{
-			sprintf_s( buffer, "Render count: %d", m_RenderCount );
-			m_FrameTimeMessage->Update( m_d3d->GetImmediateContext( ), buffer, strlen( buffer ) - 1, 1.0f, 18.0f );
-		}
-		else
-		{
-			sprintf_s( buffer, "Frame time: %.2lf", fFrameTime );
-			m_FrameTimeMessage->Update( m_d3d->GetImmediateContext( ), buffer, strlen( buffer ), 1.0f, 18.0f );
-		}
+		sprintf_s( buffer, "Frame time: %.2lf", fFrameTime );
+		m_FrameTimeMessage->Update( m_d3d->GetImmediateContext( ), buffer, strlen( buffer ), 1.0f, 18.0f );
 		if ( strlen( cheat ) > 0 )
 		{
 			sprintf_s( buffer, ":%s:", cheat );
@@ -153,6 +110,21 @@ void CGraphics::Update( bool RenderMenu, DWORD dwFramesPerSecond, float fFrameTi
 	else
 	{
 		m_Camera->Update( fFrameTime );
+		for ( int i = 0; i < 20; ++i )
+		{
+			for ( int j = 0; j < 30; ++j )
+			{
+				DirectX::XMFLOAT3 camPos = m_Camera->GetCameraPosition( );
+				DirectX::XMFLOAT3 treePos;
+				DirectX::XMStoreFloat3( &treePos,
+					DirectX::XMVector3TransformCoord( DirectX::XMVectorSet( 0.0f, 0.0f, 0.0f, 1.0f ), trees[ i * 30 + j ] )
+				);
+				float rot = atan2f( treePos.x - camPos.x, treePos.z - camPos.z );
+				trees[ i * 30 + j ] = DirectX::XMMatrixIdentity( );
+				trees[ i * 30 + j ] = DirectX::XMMatrixRotationY(rot) * 
+					DirectX::XMMatrixTranslation( ( float ) i * 3.0f, 1.0f, ( float ) j * 3 );
+			}
+		}
 	}
 
 }
@@ -165,82 +137,19 @@ void CGraphics::Frame( bool RenderMenu, DWORD dwFramesPerSecond, float fFrameTim
 
 void CGraphics::Render( bool RenderMenu, char * Cheat, UINT MouseX, UINT MouseY )
 {
-	bool RenderGlass = false;
-	m_RenderCount = 0;
 	m_Camera->Render();
-	m_Camera->RenderReflection( m_fWaterHeight );
 	m_d3d->DisableCulling( );
-
-	m_RefractionTexture->SetRenderTarget( m_d3d->GetImmediateContext( ), m_d3d->GetDepthStencilView( ) );
-	m_RefractionTexture->BeginScene( m_d3d->GetImmediateContext( ), m_d3d->GetDepthStencilView( ), common::HexToRGB( 0x000000 ) );
-
-	m_Bath->Render( m_d3d->GetImmediateContext( ) );
-	m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Bath->GetIndexCount( ), m_Bath->GetTexture( ),
-		m_Bath->GetWorld( ), m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight,
-		DirectX::XMFLOAT4( 0.0f, -1.0f, 0.0f, m_fWaterHeight ) );
-
-	m_ReflectionTexture->SetRenderTarget( m_d3d->GetImmediateContext( ), m_d3d->GetDepthStencilView( ) );
-	m_ReflectionTexture->BeginScene( m_d3d->GetImmediateContext( ), m_d3d->GetDepthStencilView( ), common::HexToRGB( 0x000000 ) );
-
-	m_Wall->Render( m_d3d->GetImmediateContext( ) );
-	m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Wall->GetIndexCount( ), m_Wall->GetTexture( ),
-		m_Wall->GetWorld( ), m_Camera->GetReflectView( ), m_Camera->GetProjection( ), Light, PointLight);
-
-	if ( m_Camera->isCubeinFrustum( 1.0f, DirectX::XMVectorGetX( m_Glass->getMiddlePoint( ) ),
-		DirectX::XMVectorGetY( m_Glass->getMiddlePoint( ) ), DirectX::XMVectorGetZ( m_Glass->getMiddlePoint( ) ) ) )
-	{
-		m_GlassRefraction->SetRenderTarget( m_d3d->GetImmediateContext( ), m_d3d->GetDepthStencilView( ) );
-		m_GlassRefraction->BeginScene( m_d3d->GetImmediateContext( ), m_d3d->GetDepthStencilView( ), common::HexToRGB( 0x0 ) );
-
-		m_Wall->Render( m_d3d->GetImmediateContext( ) );
-		m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Wall->GetIndexCount( ), m_Wall->GetTexture( ),
-			m_Wall->GetWorld( ), m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight );
-
-		m_Ground->Render( m_d3d->GetImmediateContext( ) );
-		m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Ground->GetIndexCount( ), m_Ground->GetTexture( ),
-			m_Ground->GetWorld( ), m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight );
-
-		m_Bath->Render( m_d3d->GetImmediateContext( ) );
-		m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Bath->GetIndexCount( ), m_Bath->GetTexture( ),
-			m_Bath->GetWorld( ), m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight );
-
-		m_Water->Render( m_d3d->GetImmediateContext( ) );
-		m_WaterShader->Render( m_d3d->GetImmediateContext( ), m_Water->GetIndexCount( ), m_ReflectionTexture->GetTexture( ),
-			m_RefractionTexture->GetTexture( ), m_Water->GetTexture( ), m_Camera->GetReflectView( ), m_Water->GetWorld( ),
-			m_Camera->GetView( ), m_Camera->GetProjection( ), m_fWaterTranslation, 0.03f );
-		RenderGlass = true;
-	}
-	m_d3d->EnableBackBuffer( );
 	m_d3d->BeginScene( );
-	
-	m_Wall->Render( m_d3d->GetImmediateContext( ) );
-	m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Wall->GetIndexCount( ), m_Wall->GetTexture( ),
-		m_Wall->GetWorld( ), m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight );
 
 	m_Ground->Render( m_d3d->GetImmediateContext( ) );
 	m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Ground->GetIndexCount( ), m_Ground->GetTexture( ),
 		m_Ground->GetWorld( ), m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight );
 
-	m_Bath->Render( m_d3d->GetImmediateContext( ) );
-	m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Bath->GetIndexCount( ), m_Bath->GetTexture( ),
-		m_Bath->GetWorld( ), m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight );
-
-	m_Glass->Render( m_d3d->GetImmediateContext( ) );
-	if ( RenderGlass )
-		/*m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Glass->GetIndexCount( ), m_GlassRefraction->GetTexture( ),
-			m_Glass->GetWorld( ), m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight );*/
-		m_GlassShader->Render( m_d3d->GetImmediateContext( ), m_Glass->GetIndexCount( ), m_Glass->GetTexture( ),
-			m_GlassRefraction->GetTexture( ), m_Glass->GetBumpMap( ), m_Glass->GetWorld( ), m_Camera->GetView( ),
-			m_Camera->GetProjection( ), 0.1f );
-	else
-		m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Glass->GetIndexCount( ), m_Glass->GetTexture( ),
-			m_Glass->GetWorld( ), m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight );
-
-	m_Water->Render( m_d3d->GetImmediateContext( ) );
-	m_WaterShader->Render( m_d3d->GetImmediateContext( ), m_Water->GetIndexCount( ), m_ReflectionTexture->GetTexture( ),
-		m_RefractionTexture->GetTexture( ), m_Water->GetTexture( ), m_Camera->GetReflectView( ), m_Water->GetWorld( ),
-		m_Camera->GetView( ), m_Camera->GetProjection( ), m_fWaterTranslation, 0.03f );
-
+	m_Square->Render( m_d3d->GetImmediateContext( ) );
+	for ( int i = 0; i < 600; ++i )
+		m_TextureShader->Render( m_d3d->GetImmediateContext( ), m_Square->GetIndexCount( ), m_Square->GetTexture( ),
+			trees[ i ], m_Camera->GetView( ), m_Camera->GetProjection( ), Light, PointLight );
+	
 	if (RenderMenu)
 	{
 
@@ -274,15 +183,6 @@ void CGraphics::Render( bool RenderMenu, char * Cheat, UINT MouseX, UINT MouseY 
 void CGraphics::Shutdown()
 {
 	FontClass::Shutdown();
-
-	m_RefractionTexture->Shutdown();
-	delete m_RefractionTexture;
-
-	m_ReflectionTexture->Shutdown( );
-	delete m_ReflectionTexture;
-
-	m_GlassRefraction->Shutdown( );
-	delete m_GlassRefraction;
 
 	m_Cheat->Shutdown();
 	delete m_Cheat;
@@ -328,26 +228,17 @@ void CGraphics::Shutdown()
 
 	delete Light;
 
-	m_Ground->Shutdown( );
-	delete m_Ground;
-
-	m_Bath->Shutdown( );
-	delete m_Bath;
-	
-	m_Wall->Shutdown( );
-	delete m_Wall;
-
-	m_Water->Shutdown( );
-	delete m_Water;
-
-	m_Glass->Shutdown( );
-	delete m_Glass;
-
 	m_Camera->Shutdown();
 	delete m_Camera;
 	
 	m_Cursor->Shutdown();
 	delete m_Cursor;
+
+	m_Square->Shutdown( );
+	delete m_Square;
+
+	m_Ground->Shutdown( );
+	delete m_Ground;
 	
 	m_d3d->Shutdown();
 	delete m_d3d;
--- Ceva/TexturePixelShader.hlsl
+++ Ceva/TexturePixelShader.hlsl
@@ -4,44 +4,45 @@ Texture2D ObjTexture : register ( t0 );
 struct PSIn
 {
 	float4 Pos : SV_Position;
-    float3 worldPos : POSITION;
+	float3 worldPos : POSITION;
 	float2 Tex : TEXCOORD;
 	float3 Normal : NORMAL;
-    float3 lightPos : TEXCOORD1;
+	float3 lightPos : TEXCOORD1;
 };
 
 struct DirectionalLight
 {
-    float4 Diffuse;
-    float4 Ambient;
-    float3 Direction;
+	float4 Diffuse;
+	float4 Ambient;
+	float3 Direction;
 };
 
 struct PointLight
 {
-    float3 Position;
-    float pad;
-    float4 Diffuse;
-    float3 Attenuation;
-    float Range;
+	float3 Position;
+	float pad;
+	float4 Diffuse;
+	float3 Attenuation;
+	float Range;
 };
 
 cbuffer cbSun : register ( b0 )
 {
-    DirectionalLight Sun;
+	DirectionalLight Sun;
 };
 
 cbuffer cbPointLights : register ( b1 )
 {
-    PointLight Point;
+	PointLight Point;
 };
 
 float4 main( PSIn input ) : SV_TARGET
 {
 	float4 textureColor = ObjTexture.Sample( Sampler,input.Tex );
+	clip ( textureColor.a - 0.25f );
 	float4 color = Sun.Ambient;
 	float3 lightDir = -Sun.Direction;
-    float4 color1 = float4( 0.0f, 0.0f, 0.0f, 0.0f );
+	float4 color1 = float4( 0.0f, 0.0f, 0.0f, 0.0f );
 
 	float howMuchLight = dot( input.Normal, lightDir );
 	if ( howMuchLight > 0.0f )
@@ -49,26 +50,26 @@ float4 main( PSIn input ) : SV_TARGET
 		color += ( Sun.Diffuse * howMuchLight );
 	}
 
-    color = saturate( color );
+	color = saturate( color );
 
-    float3 finalColor = float3( 0.0f, 0.0f, 0.0f );
-    float3 finalAmbient = textureColor.xyz * Sun.Ambient.xyz;
+	float3 finalColor = float3( 0.0f, 0.0f, 0.0f );
+	float3 finalAmbient = textureColor.xyz * Sun.Ambient.xyz;
 
-    float3 lightToPixelVec = Point.Position - input.worldPos;
-    float size = length( lightToPixelVec );
-    if ( size > Point.Range )
-        return textureColor * color;
+	float3 lightToPixelVec = Point.Position - input.worldPos;
+	float size = length( lightToPixelVec );
+	if ( size > Point.Range )
+		return textureColor * color;
 
-    lightToPixelVec /= size;
-    howMuchLight = dot( lightToPixelVec, input.Normal );
-    if ( howMuchLight > 0.0f )
-    {
-        finalColor += howMuchLight * Point.Diffuse.xyz * textureColor.xyz;
-        finalColor /= Point.Attenuation[0] + ( size * Point.Attenuation[1] ) + ( ( size * size ) * Point.Attenuation[2] );
-    }
+	lightToPixelVec /= size;
+	howMuchLight = dot( lightToPixelVec, input.Normal );
+	if ( howMuchLight > 0.0f )
+	{
+		finalColor += howMuchLight * Point.Diffuse.xyz * textureColor.xyz;
+		finalColor /= Point.Attenuation[0] + ( size * Point.Attenuation[1] ) + ( ( size * size ) * Point.Attenuation[2] );
+	}
 
-    finalColor = saturate( finalAmbient + finalColor + color.xyz * textureColor.xyz );
-    return float4( finalColor, 1.0f );
+	finalColor = saturate( finalAmbient + finalColor + color.xyz * textureColor.xyz );
+	return float4( finalColor, 1.0f );
 
 }
 
